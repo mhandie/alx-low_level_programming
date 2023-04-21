5ff8d60563b30d8a91a75f3df6720fe58f2197d4
mhandie <anokpara@yahoo.com>
2023-04-20 20:02:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  * main - this is the entry point
- * Return: 0 or success
+ * Return: 1 or nothing
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
