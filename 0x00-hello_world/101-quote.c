4122a0dfd2609409cd4a98668bedd9659abd46cf
mhandie <anokpara@yahoo.com>
2023-04-20 19:58:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- *  main - my functions
- * Return: always 0 or Success
+ * main - this is the entry point
+ * Return: 0 or success
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
