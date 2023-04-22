e8c7e0167aac4887b61a3b7604d647a0259885b1
mhandie <anokpara@yahoo.com>
2023-04-21 21:42:14 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - let us print all possible combinantions
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 30; i++)
+	putchar(i);
+	if (i != 29)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
