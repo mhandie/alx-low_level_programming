9d9436c6b923b41bf895bb59f0d3381e4eecb5c5
mhandie <anokpara@yahoo.com>
2023-04-21 22:59:30 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,17 +8,12 @@
  */
 int main(void)
 {
-	int num;
-	char c;
-	{
-		for (num = '0'; num < 10; num++)
-		putchar((num % 10) + '0');
-	}
-	{
-		for (c = 'a'; c <= 'f'; c++)
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
-
