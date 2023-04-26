9c16783700f814f064f36bb68ef3e3fea376d83d
mhandie <anokpara@yahoo.com>
2023-04-26 16:56:19 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,14 +17,13 @@ while (i < 24)
 j = 0;
 while (j < 60)
 {
- _putchar((i / 10) + '0');
+_putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
-
- _putchar(':');
- _putchar((j / 10) + '0');
- _putchar((j % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
- j++;
+j++;
 }
 i++;
 }
