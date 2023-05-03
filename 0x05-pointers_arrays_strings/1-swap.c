900bd0234f22dfc1823e9c3a0cb908eb182b79c7
mhandie <anokpara@yahoo.com>
2023-05-02 17:18:58 -0700

    98

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 /**
-* 1-swap - swap the code
-*
-* Return: 0 success
+ * swap_int - swap the code
+ * @a: swap to b
+ * @b: swap to a
+ * Return: 0 success
 */
 void swap_int(int *a, int *b)
 {
