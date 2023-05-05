4e25f9c9300819232bc3a63e022efe8b9b087f60
mhandie <anokpara@yahoo.com>
2023-05-04 18:13:44 -0700

    all

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,7 +4,6 @@
  * @n: integers
  * Return: 0
  */
-
 void rev_string(char *n)
 {
 int a = 0;
@@ -25,4 +24,46 @@ temp = *(n + b);
 *(n + b) = *(n + a);
 *(n + a) = temp;
 }
+}
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int overflow = 0, a = 0, b = 0, digits = 0;
+
+int val1 = 0, val2 = 0, temp_tot = 0;
+
+while (*(n1 + a) != '\0')
+a++;
+while (*(n2 + b) != '\0')
+b++;
+a--;
+b--;
+if (b >= size_r || a >= size_r)
+return (0);
+while (b >= 0 || a >= 0 || overflow == 1)
+{
+if (a < 0)
+val1 = 0;
+else
+val1 = *(n1 + a) - '0';
+if (b < 0)
+val2 = 0;
+else
+val2 = *(n2 + b) - '0';
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits >= (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+b--;
+a--;
+}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,35 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of string.
- * @str: The string is to be capitalized.
+ * cap_string - Capitalizes words of a string.
+ * @str: The string is capitalized.
  *
- * Return: A pointer to the changed string.
+ * Return: 0
  */
 char *cap_string(char *str)
 {
-int cap_next = 1;
-char *p = str;
-while (*p != '\0')
+int index = 0;
+
+while (str[index])
 {
-if (*p == ' ' || *p == '\t' || *p == '\n' ||
-*p == ',' || *p == ';' || *p == '.' || *p == '!' || *p == '?' ||
-*p == '"' || *p == '(' || *p == ')' || *p == '{' || *p == '}')
-{
-cap_next = 1;
-}
-else if (cap_next)
-if (*p >= 'a' && *p <= 'z')
-{
-*p -= 32;
-}
-cap_next = 0;
-}
-p++;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
 }
 return (str);
 }
