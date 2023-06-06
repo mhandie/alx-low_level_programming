18998f8800015023b6ca60dc263248e56e984639
mhandie <anokpara@yahoo.com>
2023-06-06 07:28:13 -0700

    all commits

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,15 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
+size_t compute = 0;
 while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-count++;
+compute++;
 }
-return (count);
+return (compute);
 }
