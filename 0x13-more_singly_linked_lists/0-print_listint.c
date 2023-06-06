5f769162a8bfcab5f9c6cf62d117a2b0b30ebfe4
mhandie <anokpara@yahoo.com>
2023-06-06 05:51:41 -0700

    all commits

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+unsigned int print_listint(const listint_t *h)
+{
+unsigned int count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+Count++;
+}
+return (count);
+}
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+Count++;
+}
+return (count);
+}
