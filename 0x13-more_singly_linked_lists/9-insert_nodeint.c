b5e68b8c7ca0a09b361b016e6900e6acb3450d87
mhandie <anokpara@yahoo.com>
2023-06-06 17:50:11 -0700

    all commits

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,30 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-if (head == NULL)
+unsigned int i;
+listint_t *a;
+listint_t *tmp = *head;
+a = malloc(sizeof(listint_t));
+if (!a || !head)
 return (NULL);
-listint_t *new_code = (listint_t *)malloc(sizeof(listint_t));
-if (new_code == NULL)
-return (NULL);
-new_code->next = n;
+a->n = n;
+a->next = NULL;
 if (idx == 0)
 {
-new_code->next = *head;
-*head = new_code;
+a->next = *head;
+*head = a;
+return (a);
+}
+for (i = 0; tmp && i < idx; i++)
+{
+if (i == idx - 1)
+{
+a->next = tmp->next;
+tmp->next = a;
+return (a);
 }
 else
-{
-listint_t *current = *head;
-unsigned int i = 0;
-while (current != NULL && i < idx - 1)
-{
-current = current->next;
-i++;
+tmp = tmp->next;
 }
-if (current == NULL || current->next == NULL)
-{
-free(new_code);
 return (NULL);
-}
-new_code->next = current->next;
-current->next = new_code;
-}
-return (new_code);
 }
