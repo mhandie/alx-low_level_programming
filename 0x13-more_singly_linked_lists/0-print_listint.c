9362ae436f083387c9fc0eaf8e51d55b11388a94
mhandie <anokpara@yahoo.com>
2023-06-06 17:02:56 -0700

    all commits

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,31 +2,15 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-size_t free_listint_safe(listint_t **h)
+size_t print_listint(const listint_t *h)
 {
-size_t len = 0;
-int diff;
-listint_t *tmp;
-if (!h || !*h)
-return (0);
-while (*h)
+size_t number = 0;
+
+while (h)
 {
-diff = *h - (*h)->next;
-if (diff > 0)
-{
-tmp = (*h)->next;
-free(*h);
-*h = tmp;
-len++;
+printf("%d\n", h->n);
+number++;
+h = h->next;
 }
-else
-{
-free(*h);
-*h = NULL;
-len++;
-break;
-}
-}
-*h = NULL;
-return (len);
+return (number);
 }
