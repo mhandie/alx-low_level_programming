c9cfc5712a2e2fe76ae05f38cf72b9516094467f
mhandie <anokpara@yahoo.com>
2023-06-08 14:39:36 -0700

    all files

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ int a, count = 0;
 unsigned long int current;
 unsigned long int exclusive = n ^ m;
 
-for (a = 98; a >= 0; a--)
+for (a = 63; a >= 0; a--)
 {
 current = exclusive >> a;
 if (current & 1)
